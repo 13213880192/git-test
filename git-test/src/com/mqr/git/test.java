a6a512d24d7be7d0fca4df29dc8ffeea541b965f
mengqingrui <mqrlife@gmail.com>
2020-11-07 18:21:01 +0800

    第一次把本地工程添加到git

---
+++ git-test/src/com/mqr/git/test.java
@@ -0,0 +1,11 @@
+package com.mqr.git;
+
+/**
+ * @author mqr
+ * @version 1.0
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
