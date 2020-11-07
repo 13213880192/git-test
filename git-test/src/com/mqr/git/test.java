a37b7129c101b5e11bdb37e345321bc36fcc3406
mengqingrui <mqrlife@gmail.com>
2020-11-07 18:22:18 +0800

    第一次修改添加test方法

--- git-test/src/com/mqr/git/test.java
+++ git-test/src/com/mqr/git/test.java
@@ -8,4 +8,7 @@ public class test {
     public static void main(String[] args) {
         System.out.println("hello git");
     }
+    public void test(){
+        System.out.println(1+1);
+    }
 }
